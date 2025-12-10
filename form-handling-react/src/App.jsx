906d0b050bc2a28c7668faa04740f6372c1815cb
Andy Iroko <andy.iroko.si@gmail.com>
2025-12-10 18:58:50 +0000

    update formik form component

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import formikForm from "./components/formikForm";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,7 +32,7 @@ function App() {
       </p>
       <div>
         <RegistrationForm />
-        <FormikForm />
+        <formikForm />
       </div>
     </>
   )
